95cb01d5cc2d3e969f14455b535bd84ce43277c0
J-E Castagnede <j.e.castagnede@gmail.com>
2022-06-03 11:11:32 +0200

    concert zoning views

--- geotrek/common/functions.py
+++ geotrek/common/functions.py
@@ -1,7 +1,11 @@
-from django.contrib.gis.db.models.functions import GeoFunc
+from django.contrib.gis.db.models.functions import GeoFunc, GeomOutputGeoFunc
 from django.db.models import FloatField
 
 
 class Length(GeoFunc):
     """ ST_Length postgis function """
     output_field = FloatField()
+
+
+class SimplifyPreserveTopology(GeomOutputGeoFunc):
+    pass
